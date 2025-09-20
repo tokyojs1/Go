7bde971c30ca7cd05897676d55fc7caf7d0d4a47
Tokyo <142293678+tokyojs1@users.noreply.github.com>
2025-09-20 19:33:49 +0300

    Update power.go

--- power.go
+++ power.go
@@ -10,8 +10,8 @@ import (
 
 func main() {
     
-    username := "خلي يوزرك" 
-    password := "خلي الباسورد"
+    username := "" 
+    password := ""
 
     
     data := url.Values{}
@@ -20,32 +20,32 @@ func main() {
     
     req, err := http.NewRequest("POST", "https://www.instagram.com/accounts/login/ajax/", bytes.NewBufferString(data.Encode()))
     if err != nil {
-        fmt.Println("ايرور هيو يمعود:", err)
+        fmt.Println("Err:", err)
         return
     }
 
     req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
     req.Header.Set("User-Agent", "Mozilla/5.0")
-    req.Header.Set("X-CSRFToken", "خلي CSRF Token") 
+    req.Header.Set("X-CSRFToken", "") // CSRF
 
     client := &http.Client{}
     resp, err := client.Do(req)
     if err != nil {
-        fmt.Println("ايرور يمعود:", err)
+        fmt.Println("Err:", err)
         return
     }
     defer resp.Body.Close()
 
     body, err := ioutil.ReadAll(resp.Body)
     if err != nil {
-        fmt.Println("شوف الأيرور لتدوخني", err)
+        fmt.Println("Err", err)
         return
     }
 
     
     if resp.StatusCode == 200 {
-        fmt.Println("سجلت دخول")
+        fmt.Println("Done")
     } else {
-        fmt.Println("ماكدرت اسجل وداعتك اكو مشكله")
+        fmt.Println("get out")
     }
 }
